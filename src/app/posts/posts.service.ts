eabaceea5615bfc3a44cc118914fc5cd51750d08
Nima <nickyonzon@hotmail.com>
2020-01-07 02:28:15 -0600

    upload test

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,16 +44,21 @@ export class PostsService {
     ('http://localhost:3000/api/posts/' + id);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title, content };
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
     this.http
       .post<{ message: string; postId: string }>(
         'http://localhost:3000/api/posts',
-        post
+        postData
       )
       .subscribe(responseData => {
-        const id = responseData.postId;
-        post.id = id;
+        const post: Post = {
+          id: responseData.postId,
+          title,
+          content };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
